3dd2070c6fb1f2b9df29694ac0a5123b6245d1a5
viiidra <79606458+viiidra@users.noreply.github.com>
2022-11-01 10:43:25 +0200

    Update RelationDB_DetailedDataSchema.sql

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -1,1 +1,22 @@
-
+CREATE TABLE user(
+	user_id INT PRIMARY KEY,
+	user_name VARCHAR NOT NULL,
+	user_surname VARCHAR NOT NULL,
+  gender VARCHAR NOT NULL,
+  user_email VARCHAR NOT NULL,
+  purchase_status VARCHAR NOT NULL
+);
+CREATE TABLE purchase(
+	purchase_id INT PRIMARY KEY,
+	user_id INT REFERENCES user(user_id),
+	purchase_name VARCHAR NOT NULL,
+	price DECIMAL(10, 2) NOT NULL,
+  purchase_time SMALLDATETIME NOT NULL
+);
+CREATE TABLE schedule(
+	day_id INT PRIMARY KEY,
+  user_id INT REFERENCES user(user_id),
+  training_date DATE,
+  training_time TIME,
+  dance_type VARCHAR
+);
